0b047b3b3a66513b3c89f6365067205e1d74c8a2
NitishAndMayank <emailsender936@gmail.com>
2022-12-07 19:12:34 +0530

    Updates

---
+++ precache-manifest.bc6ebbad25f1bf6a6ad043c81b7b8b36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32f0d5b85978405b0b0015af4c555d16",
+    "url": "/TrainingImageSearch/index.html"
+  },
+  {
+    "revision": "b7492f38ad05ab53892b",
+    "url": "/TrainingImageSearch/static/css/main.30f1b11e.chunk.css"
+  },
+  {
+    "revision": "08b3c966dab06e4190cd",
+    "url": "/TrainingImageSearch/static/js/2.d26f9645.chunk.js"
+  },
+  {
+    "revision": "971b4e562d3abe5fe0995509ffca7edb",
+    "url": "/TrainingImageSearch/static/js/2.d26f9645.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7492f38ad05ab53892b",
+    "url": "/TrainingImageSearch/static/js/main.791cb7af.chunk.js"
+  },
+  {
+    "revision": "e831ba0a9216b93115c2",
+    "url": "/TrainingImageSearch/static/js/runtime-main.7486b92f.js"
+  }
+]);
